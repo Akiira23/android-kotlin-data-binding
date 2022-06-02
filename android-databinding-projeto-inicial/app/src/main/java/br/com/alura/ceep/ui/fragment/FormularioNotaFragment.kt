471a007ff439eb4e8b0f636008aff239d60be68f
Andre Akira <aah950@hotmail.com>
2022-06-02 08:57:35 -0300

    one-way e two-way data binding

--- android-databinding-projeto-inicial/app/src/main/java/br/com/alura/ceep/ui/fragment/FormularioNotaFragment.kt
+++ android-databinding-projeto-inicial/app/src/main/java/br/com/alura/ceep/ui/fragment/FormularioNotaFragment.kt
@@ -40,7 +40,6 @@ class FormularioNotaFragment : Fragment() {
         findNavController()
     }
     private lateinit var notaEncontrada: Nota
-    private var urlAtual: String = ""
     private val campoTitulo: EditText by lazy {
         formulario_nota_titulo
     }
@@ -49,12 +48,6 @@ class FormularioNotaFragment : Fragment() {
     }
     private val campoFavorita: CheckBox by lazy {
         formulario_nota_favorita
-    }
-    private val campoImagem: ImageView by lazy {
-        formulario_nota_imagem
-    }
-    private val fabAdicionaImagem: FloatingActionButton by lazy {
-        formulario_nota_fab_adiciona_imagem
     }
     private lateinit var viewDataBinding: FormularioNotaBinding
 
@@ -70,14 +63,15 @@ class FormularioNotaFragment : Fragment() {
     ): View? {
 
         viewDataBinding = FormularioNotaBinding.inflate(inflater, container, false)
+        viewDataBinding.solicitaImagem = View.OnClickListener {
+            solicitaImagem()
+        }
         return viewDataBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         tentaBuscarNota()
-        configuraFab()
-        configuraCampoImagem()
     }
 
     private fun tentaBuscarNota() {
@@ -97,44 +91,15 @@ class FormularioNotaFragment : Fragment() {
 
     private fun temIdValido() = notaId != 0L
 
-    private fun configuraFab() {
-        fabAdicionaImagem.setOnClickListener {
-            solicitaImagem()
-        }
-    }
-
-    private fun configuraCampoImagem() {
-        configuraVisibilidadeDeComponentes()
-        campoImagem.setOnClickListener {
-            solicitaImagem()
-        }
-    }
-
     private fun solicitaImagem() {
-        CarregaImagemDialog().mostra(requireContext(), urlAtual) { urlNova ->
-            this.urlAtual = urlNova
-            configuraImagem()
-        }
-    }
-
-    private fun configuraVisibilidadeDeComponentes() {
-        if (urlAtual.isEmpty()) {
-            campoImagem.visibility = GONE
-            fabAdicionaImagem.show()
-        } else {
-            campoImagem.visibility = VISIBLE
-            fabAdicionaImagem.hide()
+        CarregaImagemDialog().mostra(requireContext(), this.notaEncontrada.imagemUrl) { urlNova ->
+            this.notaEncontrada.imagemUrl = urlNova
+            viewDataBinding.nota = notaEncontrada
         }
     }
 
     private fun inicializaNota(notaEncontrada: Nota) {
         this.notaEncontrada = notaEncontrada
-        urlAtual = this.notaEncontrada.imagemUrl
-    }
-
-    private fun configuraImagem() {
-        configuraVisibilidadeDeComponentes()
-        campoImagem.carregaImagem(urlAtual)
     }
 
     private fun appBarParaEdicao() = ComponentesVisuais(appBar = AppBar(titulo = "Editando nota"))
@@ -149,21 +114,13 @@ class FormularioNotaFragment : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         if (item?.itemId == R.id.formulario_nota_menu_salva) {
             val notaCriada = criaNota()
-            salva(notaCriada)
+            notaCriada?.let { salva(notaCriada) }
         }
         return super.onOptionsItemSelected(item)
     }
 
-    private fun criaNota(): Nota {
-        val titulo = formulario_nota_titulo.text.toString()
-        val descricao = formulario_nota_descricao.text.toString()
-        val favorita = formulario_nota_favorita.isChecked
-        return notaEncontrada.copy(
-            titulo = titulo,
-            descricao = descricao,
-            favorita = favorita,
-            imagemUrl = urlAtual
-        )
+    private fun criaNota(): Nota? {
+        return viewDataBinding.nota?.copy(id = notaEncontrada.id)
     }
 
     private fun salva(notaNova: Nota) {
